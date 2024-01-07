6fada308b8c5062c7b152ef2a9c46036710c4ae9
Bernardo Frija <1903bernardo@gmail.com>
2024-01-07 21:11:58 +0000

    first version of onClick animation play

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import * as THREE from 'three';
 const cena = new THREE.Scene();
 
 const meuCanvas = document.getElementById( 'meuCanvas' )
-let renderer = new THREE.WebGLRenderer( { canvas: meuCanvas } )
+const renderer = new THREE.WebGLRenderer( { canvas: meuCanvas } )
 const width = 800
 const height = 600
 
@@ -15,74 +15,69 @@ renderer.setSize(width, height)
 renderer.shadowMap.enabled = true
 
 // criar uma camara... 
-let camara = new THREE.PerspectiveCamera( 70, width / height, 0.01, 1000 ); 
-camara.position.set(6,4,7);
-camara.lookAt(0,0,0)
+const camara = new THREE.PerspectiveCamera( 70, width / height, 0.01, 1000 ); 
+const controlos = new OrbitControls( camara, renderer.domElement);
+camara.position.set(1.5,2,5);
+camara.lookAt(0,0,0);
+controlos.update();
 
 
-export const clickableObjects = ['Porta_L','Porta_R','Gaveta_L','Gaveta_R']
-export let objects = []
+
+const clickableObjects = ['Porta_L','Porta_R','Gaveta_L','Gaveta_R']
+var objects = []
 let playedAnimation = [false,false,false,false]
 
 let raycaster = new THREE.Raycaster()
 let rato = new THREE.Vector2()
 
-// let luz = new THREE.PointLight( "white" )
-// luz.position.set(5,3,5)
-// luz.castShadow =true
-// cena.add(luz)
 
-let eixos = new THREE.AxesHelper()
-cena.add(eixos)
+// let eixos = new THREE.AxesHelper()
+// cena.add(eixos)
 
-let grelha = new THREE.GridHelper();
-cena.add(grelha);
+// let grelha = new THREE.GridHelper();
+// cena.add(grelha);
  
-let controlos = new OrbitControls( camara, renderer.domElement);
 
 let misturador = new THREE.AnimationMixer(cena);
 
-let acaoPortaDir
-let acaoPortaEsq;
+let acaoPortaDir = null;
+let acaoPortaEsq = null;
+let acaoGavetaDir = null;
+let acaoGavetaEsq = null;
 let carregador = new GLTFLoader();
 carregador.load(
     'vintageDesk.gltf',
     ( gltf ) => {
         cena.add(gltf.scene)
 
-        clipe1 = THREE.AnimationClip.findByName( gltf.animations, 'PortaDirAbrir.001' ) 
+        let clipe1 = THREE.AnimationClip.findByName( gltf.animations, 'PortaDirAbrir.001' ) 
         console.log(clipe1)
         acaoPortaDir = misturador.clipAction( clipe1 ) 
-        acaoPortaDir.clampWhenFinished = true
+        acaoPortaDir.clampWhenFinished = true;
 
-        clipe2 = THREE.AnimationClip.findByName( gltf.animations, 'PortaEsqAbrir.001' ) 
+        let clipe2 = THREE.AnimationClip.findByName( gltf.animations, 'PortaEsqAbrir.001' ) 
         console.log(clipe2)
         acaoPortaEsq = misturador.clipAction( clipe2 ) 
-        acaoPortaEsq.clampWhenFinished = true
 
-        clipe3 = THREE.AnimationClip.findByName( gltf.animations, 'GavetaDirAbrir.001' ) 
+        let clipe3 = THREE.AnimationClip.findByName( gltf.animations, 'GavetaDirAbrir.001' ) 
         console.log(clipe3)
         acaoGavetaDir = misturador.clipAction( clipe3 ) 
-        acaoGavetaDir.clampWhenFinished = true
 
-        clipe4 = THREE.AnimationClip.findByName( gltf.animations, 'GavetaEsqAbrir.001' ) 
+        let clipe4 = THREE.AnimationClip.findByName( gltf.animations, 'GavetaEsqAbrir.001' ) 
         console.log(clipe4)
         acaoGavetaEsq = misturador.clipAction( clipe4 ) 
-        acaoGavetaEsq.clampWhenFinished = true
 
         cena.traverse((x) => {
-            // if (x.isMesh) {
-            //     x.castShadow = true
-            //     x.receiveShadow = true
-
-            //     if (clickableObjects.includes(x.name))
-            //         alvo = x
-            // }
+            if (x.isMesh) {
+                x.castShadow = true
+                x.receiveShadow = true
+            }
 
             if (clickableObjects.includes(x.name)) {
                 objects.push(x);
             }
         })
+        console.log(objects)
         
     }
 )
@@ -93,11 +88,12 @@ const  latencia_minima = 1 / 60;
 function animar() {
     requestAnimationFrame(animar);
 
-    delta += relogio.getDelta();    // acumula tempo que passou desde a ultima chamada de getDelta
+    // delta += relogio.getDelta();    // acumula tempo que passou desde a ultima chamada de getDelta
 
-    if (delta  < latencia_minima)   // não exceder a taxa de atualização máxima definida
-        return;                     
+    // if (delta  < latencia_minima)   // não exceder a taxa de atualização máxima definida
+    //     return;                     
 
+    controlos.update();
     renderer.render(cena,camara);
 
     misturador.update( relogio.getDelta() ) 
@@ -105,6 +101,7 @@ function animar() {
 }
 
 animar();
+
 
 function luzes(cena) {
     /* luzes... */
@@ -126,6 +123,7 @@ function luzes(cena) {
     luzDirecional.position.set( 3, 2, 0 ); //aponta na direção de (0, 0, 0)
     luzDirecional.intensity= 30
     cena.add( luzDirecional );
+
     // auxiliar visual
     const lightHelper2 = new THREE.DirectionalLightHelper( luzDirecional, 0.2 )
     cena.add( lightHelper2 )
@@ -133,41 +131,42 @@ function luzes(cena) {
 
 luzes(cena)
 
-function pegarObjeto() {
+function pegarObjeto(objects) {
     raycaster.setFromCamera(rato, camara)
 
     let intersetados = raycaster.intersectObjects(objects);
-
-    console.log("alvo : \n")
-    console.log(intersetados[0].name)
 
     if (intersetados.length > 0) {
         // alvo.material.color = intersetados[0].object.material.color;
         // playAnimation(intersetados[0].name)
         console.log("alvo : \n")
-        console.log(intersetados[0].name)
+        if (intersetados[0].object.parent.name !== undefined) {
+            console.log(intersetados[0].object.parent.name)
+            return intersetados[0].object.parent.name;
+        }
 
     }
 }
 
 function playAnimation(name) {
+    console.log("Nome: " + name);
     switch (name) {
         case 'Porta_L':
             if (playedAnimation[0] === false) {
                 acaoPortaEsq.play();
                 playedAnimation[0] = true;
             } else {
-                acaoPortaEsq.play();
+                acaoPortaEsq.stop();
                 playedAnimation[0] = false;
             }
             break;
 
-        case 'Porta_D':
+        case 'Porta_R':
             if (playedAnimation[1] === false) {
                 acaoPortaDir.play();
                 playedAnimation[1] = true;
             } else {
-                acaoPortaDir.play();
+                acaoPortaDir.stop();
                 playedAnimation[1] = false;
             }
             break;
@@ -177,21 +176,26 @@ function playAnimation(name) {
                 acaoGavetaEsq.play();
                 playedAnimation[2] = true;
             } else {
-                acaoGavetaEsq.play();
+                acaoGavetaEsq.stop();
                 playedAnimation[2] = false;
             }
             break;
 
-        case 'Gaveta_D':
+        case 'Gaveta_R':
             if (playedAnimation[3] === false) {
                 acaoGavetaDir.play();
                 playedAnimation[3] = true;
             } else {
-                acaoGavetaDir.play();
+                acaoGavetaDir.stop();
                 playedAnimation[3] = false;
             }
             break;
+        default:
+            console.log("default case");
+            break;
     }
+    console.log("Played Animations");
+    console.log(playedAnimation);
     
     
 }
@@ -227,8 +231,9 @@ menu_loop.addEventListener("change", () => {
     switch(menu_loop.value) {
         case "1":
             // alert("once");
-            acao.clampWhenFinished = -acao.clampWhenFinished;
+            acaoPortaDir.clampWhenFinished = -acao.clampWhenFinished;
             acaoPortaDir.setLoop(THREE.LoopOnce);
+            acaoPortaEsq.setLoop(THREE.LoopOnce);
 
             acao.clampWhenFinished = -acao.clampWhenFinished;
             acaoPortaEsq.setLoop(THREE.LoopOnce);
@@ -247,9 +252,21 @@ menu_loop.addEventListener("change", () => {
 },false);
 
 
-botaoPlay.addEventListener("click",() => { acaoPortaDir.play(); acaoPortaEsq.play() });
 
-botaoStop.addEventListener("click",() => { acaoPortaDir.stop(); acaoPortaEsq.stop() });
+botaoPlay.addEventListener("click",() => {
+    acaoPortaDir.play(); 
+    acaoPortaEsq.play();
+    acaoGavetaDir.play();
+    acaoGavetaEsq.play();
+});
+
+botaoStop.addEventListener("click",() => {
+    acaoPortaDir.stop();
+    acaoPortaEsq.stop();
+    acaoGavetaDir.stop();
+    acaoGavetaEsq.stop();
+
+});
 
 botaoPause.addEventListener("click",() => {
     acaoPortaDir.paused = !acaoPortaDir.paused;
@@ -263,8 +280,27 @@ botaoReverse.addEventListener("click",() => {
 
 
 meuCanvas.addEventListener("click", (evento) => {
-    rato.x = (evento.clientX / width) * 2 - 1
-    rato.y = -(evento.clientY / height) * 2 + 1
+    let limites = evento.target.getBoundingClientRect();
+
+    // rato.x = (evento.clientX / width) * 2 - 1
+    // rato.y = -(evento.clientY / height) * 2 + 1
+    rato.x = 2 * (evento.clientX - limites.left) / parseInt(meuCanvas.style.width) - 1;
+    rato.y = 1 - 2 * (evento.clientY - limites.top) / parseInt(meuCanvas.style.height);
     console.log("x: " + rato.x + "\n" + "y: " + rato.y + "\n");
-    pegarObjeto();
+    let nomeObjeto = pegarObjeto(objects);
+    playAnimation(nomeObjeto)
+
+    // raycaster.setFromCamera(rato, camara)
+
+    // const intersetados = raycaster.intersectObjects(objects);
+    // console.log("Objects:");
+    // console.log(objects);
+
+    // if (intersetados.length > 0) {
+    //     // alvo.material.color = intersetados[0].object.material.color;
+    //     // playAnimation(intersetados[0].name)
+    //     console.log("alvo : \n")
+    //     console.log(intersetados[0].object.parent.name)
+
+    // }
 });
